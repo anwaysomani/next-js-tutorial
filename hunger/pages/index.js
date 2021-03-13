924ba81d2c6024b274d0507f2e5c01170227174a
Anway <31155434+anwaysomani@users.noreply.github.com>
2021-03-13 16:09:22 +0530

    Updated index to support landing view for home poage

--- hunger/pages/index.js
+++ hunger/pages/index.js
@@ -1,17 +1,41 @@
 import Head from 'next/head'
 import Navigation from "../components/navigation";
+import Diffy from "../components/diffy";
+import styles from '../styles/index.module.css'
 
 export default function Home() {
+  // var diffy_render = {
+  //
+  // }
+
   return (
-    <div className='background-image'>
-      <Head>
-        <title>La Faim</title>
-        <link rel="icon" href="/favicon.ico"/>
-      </Head>
-      <Navigation>
+    <div>
+      <div className='background-image w-100 h-100 text-white'>
+        <Head>
+          <title>La Faim</title>
+          <link rel="icon" href="/favicon.ico"/>
+        </Head>
+        <Navigation/>
+        <hr/>
 
-      </Navigation>
+        {/* restaurant title */}
+        <div className='text-upper'>
+          <div className='text-center top-40 position-absolute w-100 font-2-5'>Restaurant</div>
+          <div className='text-center position-absolute w-100 top-45 font-7'>Hungry People</div>
+          <div className='w-100 top-67 position-absolute'>
+            <hr className={['w-10 ', styles.hr_border_2].join(' ')}/>
+          </div>
+        </div>
+      </div>
 
+      {/* About Us */}
+      {/*<div className='background-white w-100 h-100'>*/}
+      {/*  <Diffy children={[{*/}
+      {/*    'title': 'Sample Title'},*/}
+      {/*    {*/}
+      {/*      'title': 'Sample 2 Title'*/}
+      {/*    }]} />*/}
+      {/*</div>*/}
     </div>
   )
 }
