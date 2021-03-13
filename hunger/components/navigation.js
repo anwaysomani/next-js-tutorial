79ae354f9fe86f7ca9ecb7e672869990bda48ca1
Anway <31155434+anwaysomani@users.noreply.github.com>
2021-03-13 16:08:32 +0530

    Added navigation component

--- hunger/components/navigation.js
+++ hunger/components/navigation.js
@@ -1,12 +1,44 @@
 import styles from './navigation.module.css'
 
 export default function Navigation({ children }) {
-  var x = []
-  for(var i = 0; i < 10; i++) {
-    x.push(i)
+  var nav_items = [
+    {
+      title: 'Home'
+    },
+    {
+      title: 'About'
+    },
+    {
+      title: 'Team'
+    },
+    {
+      title: 'Booking'
+    },
+    {
+      title: 'Menu'
+    },
+    {
+      title: 'Galerie'
+    },
+    {
+      title: 'Events'
+    },
+    {
+      title: 'Contact'
+    }
+  ];
+
+  var entity = [];
+  for(var i=0; i<nav_items.length; i++) {
+    entity.push(
+      <div className={[styles.font_12, styles.nav_item, styles.m_0, styles.pull_left, styles.f_w_900].join(" ")}>
+        <span>{nav_items[i].title}</span>
+      </div>
+    )
   }
 
-  return <div className={styles.nav_container}>
-    <span className={styles.container}>{x}</span>
+
+  return <div className={styles.nav_container} >
+    <div className={styles.container}>{entity}</div>
   </div>
 }
